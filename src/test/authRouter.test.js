1e7db620a6d1c32a59682c3a7a3b042533364951
Max Miller <maxrmiller55@gmail.com>
2026-01-27 16:14:10 -0700

    added github CI pipeline

--- src/test/authRouter.test.js
+++ src/test/authRouter.test.js
@@ -1,6 +1,17 @@
 const request = require('supertest');
 const app = require('../service');
 
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
@@ -10,11 +21,13 @@ beforeAll(async () => {
   testUserAuthToken = registerRes.body.token;
 });
 
+
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
-  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  const user = { ...testUser, roles: [{ role: 'diner' }] };
+  delete user.password; 
   expect(loginRes.body.user).toMatchObject(user);
 });
