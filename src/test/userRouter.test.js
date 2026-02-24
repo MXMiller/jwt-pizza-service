4de8144c6d2e01db8dafbd94b59242a1d8edcd84
Max Miller <maxrmiller55@gmail.com>
2026-02-24 15:50:59 -0700

    more lint

--- src/test/userRouter.test.js
+++ src/test/userRouter.test.js
@@ -197,7 +197,7 @@ describe('userRouter.js tests', () => {
   });
 
   test('admin delete user', async () => {
-    const [user, userToken] = await registerUser(request(app));
+    const [userToken] = await registerUser(request(app));
     const listUsersRes = await request(app)
       .delete('/api/user/10')
       .set('Authorization', 'Bearer ' + userToken);
