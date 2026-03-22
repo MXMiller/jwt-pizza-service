0c00986a8d5db1df62d087745cb77c3093c61986
Max Miller <maxrmiller55@gmail.com>
2026-03-22 17:15:55 -0600

    changed bback to class

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,7 @@
 const config = require('./config');
 
-function httpLogger(req, res, next) {
+class Logger {
+  httpLogger = (req, res, next) => {
     let send = res.send;
     res.send = (resBody) => {
       const logData = {
@@ -11,49 +12,50 @@ function httpLogger(req, res, next) {
         reqBody: JSON.stringify(req.body),
         resBody: JSON.stringify(resBody),
       };
-      const level = statusToLogLevel(res.statusCode);
-      log(level, 'http', logData);
+      const level = this.statusToLogLevel(res.statusCode);
+      this.log(level, 'http', logData);
       res.send = send;
       return res.send(resBody);
     };
     next();
   };
 
-function log(level, type, logData) {
-  const labels = { component: config.logging.source, level: level, type: type };
-  const values = [nowString(), sanitize(logData)];
-  const logEvent = { streams: [{ stream: labels, values: [values] }] };
+  log(level, type, logData) {
+    const labels = { component: config.logging.source, level: level, type: type };
+    const values = [this.nowString(), this.sanitize(logData)];
+    const logEvent = { streams: [{ stream: labels, values: [values] }] };
 
-  sendLogToGrafana(logEvent);
+    this.sendLogToGrafana(logEvent);
+  }
+
+  statusToLogLevel(statusCode) {
+    if (statusCode >= 500) return 'error';
+    if (statusCode >= 400) return 'warn';
+    return 'info';
+  }
+
+  nowString() {
+    return (Math.floor(Date.now()) * 1000000).toString();
+  }
+
+  sanitize(logData) {
+    logData = JSON.stringify(logData);
+    return logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
+  }
+
+  sendLogToGrafana(event) {
+    const body = JSON.stringify(event);
+    fetch(`${config.logging.endpointUrl}`, {
+      method: 'post',
+      body: body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${config.logging.accountId}:${config.logging.apiKey}`,
+      },
+    }).then((res) => {
+      if (!res.ok) console.log('Failed to send log to Grafana');
+    });
+  }
 }
 
-function statusToLogLevel(statusCode) {
-  if (statusCode >= 500) return 'error';
-  if (statusCode >= 400) return 'warn';
-  return 'info';
-}
-
-function nowString() {
-  return (Math.floor(Date.now()) * 1000000).toString();
-}
-
-function sanitize(logData) {
-  logData = JSON.stringify(logData);
-  return logData.replace(/\\"password\\":\s*\\"[^"]*\\"/g, '\\"password\\": \\"*****\\"');
-}
-
-function sendLogToGrafana(event) {
-  const body = JSON.stringify(event);
-  fetch(`${config.logging.endpointUrl}`, {
-    method: 'post',
-    body: body,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${config.logging.accountId}:${config.logging.apiKey}`,
-    },
-  }).then((res) => {
-    if (!res.ok) console.log('Failed to send log to Grafana');
-  });
-}
-
-module.exports = ( httpLogger );
+module.exports = new Logger();
