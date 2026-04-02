c4642393b76355c0d054737a100049f1027870ac
Max Miller <maxrmiller55@gmail.com>
2026-04-01 20:17:57 -0600

    more factory http info

--- src/logger.js
+++ src/logger.js
@@ -31,7 +31,14 @@ class Logger {
   }
 
   factoryLogHelper(fReqUrl, fReqBody, fRes){
-    this.log('info', 'factory', { path: fReqUrl, reqBody: fReqBody, resBody: fRes });
+    this.log('info', 'factory', { 
+      authorized: !!fReqBody.headers.authorization,
+      path: fReqUrl,
+      method: fReqBody.method,
+      statusCode: fReqBody.statusCode,
+      reqBody: fReqBody, 
+      resBody: fRes 
+    });
   }
 
   log(level, type, logData) {
